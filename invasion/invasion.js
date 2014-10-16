0de9ee9b542948c80518c3a372de2e2d22e487c4
misantronic <david.skx@posteo.de>
2014-10-16 21:28:14 +0200

    reduced invasion.min.js to 512b

--- invasion/invasion.js
+++ invasion/invasion.js
@@ -1,8 +1,8 @@
-y=x=20;p=y*x-x/2;$=l=t=0;n="indexOf";c="...";
-for(b=[],j=2;j<136;j+=2) {
-	b.push(j);j==14&&(j=41)||j==55&&(j=80)||j==94&&(j=121)
-}
-onkeydown=function(e){(k=e.which)==39?p++:k==37?p--:!l&&(l=p)};
+y=x=20;p=389;$=l=t=0;n="indexOf";c="...";
+for(b=[],j=2;j<136;j+=2)b.push(j),j==14&&(j=41)||j==55&&(j=80)||j==94&&(j=121)
+onkeydown=function(e){
+	(k=e.which)==39?p++:k==37?p--:!l&&(l=p)
+};
 setInterval(function(){
 	_="<pre>";if(l)(l-=y)<0&&(l=0);
 	for(i=0;i<x*y;i++){
@@ -12,6 +12,6 @@ setInterval(function(){
 		_+=i==p?"oIo":~b[n](i)?" x ":i==l&&l?" | ":c
 	}
 	document.body.innerHTML=_+="\n.P "+$;
-	for(var i=0;i<b.length;i++)b[i]+=t^0&&t%5e3==0?y:t%2e3==0?1:t%1e3==0&&(-1);
+	for(i=0;i<28;i++)b[i]+=t^0&&t%5e3==0?y:t%2e3==0?1:t%1e3==0&&(-1);
 	t+=o
 },o=50);
