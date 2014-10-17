b0349069f2b0342b7db971bfc28803e610600e30
misantronic <david.skx@posteo.de>
2014-10-17 18:55:34 +0200

    minifying

--- invasion/invasion.js
+++ invasion/invasion.js
@@ -2,8 +2,7 @@ p=389;$=l=m=t=0;c=" _ ";
 onkeydown=function(e){(k=e.which)==39?p++:k==37?p--:!l&(l=p)};
 setInterval(function(){
 	_="<pre>";l&(l-=20)<0&&(l=0);
-	m&&(m+=20)>399&&(m=0);
-	if(!m)m=b[+new Date%6];
+	m=(m+=20)>p?b[+new Date%6]:m;
 	for(i=0;i<400;i++){
 		if(i%20==0)_+="\n";
 		if(~b[n="indexOf"](l))b.splice(b[n](l),1),$+=25,l=0;
@@ -12,6 +11,6 @@ setInterval(function(){
 	}
 	document.body.innerHTML=_+="\nP "+$;
 	t+=o;
-	for(i in b)b[i]+=t%5e3==0?20:t%2e3==0?1:t%1e3==0&&(-1)
+	for(i in b)b[i]+=t%5e3==0?20:t%2e3==0?1:t%1e3==0&&-1
 },o=50);
 for(b=[],j=2;j<136;j+=2)b.push(j),j==14&&(j=41)||j==55&&(j=80)||j==94&&(j=121)
