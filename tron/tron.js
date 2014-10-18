29de36f3005eaa7dce80cfb70b924caad394127b
misantronic <david.skx@posteo.de>
2014-10-18 22:19:25 +0200

    minifiying

--- tron/tron.js
+++ tron/tron.js
@@ -1,5 +1,5 @@
-p={x:795,y:400};w=[];d=1;
-P={x:5,y:0};W=[];D=3;
+w=[{x:795,y:400}];d=1;// player 1
+W=[{x:5,y:0}];D=3;// player 2
 with(c.getContext("2d")){
 	onkeyup=function(e){
 		(k=e.which)==39?d=2:k==37?d=4:k==38?d=1:k==40?d=3:k==87?D=1:k==68?D=2:k==83?D=3:k==65&&(D=4)
@@ -7,29 +7,32 @@ with(c.getContext("2d")){
 
 	v=setInterval('\
 		c.width=c.width;\
-		z(p,w,d,W,"blue",0);\
-		z(P,W,D,w,"red",1)\
+		z(w,d,W,"blue",0);\
+		z(W,D,w,"red",1)\
 	',60);
 
-	// h: player
 	// f: walls
 	// g: direction
 	// F: enemy wall
 	// c: color
 	// b: start index (for gradient)
-	z=function(h,f,g,F,c,b) {
-		f.push({x:h.x,y:h.y});
+	z=function(f,g,F,c,b){
+		t=function(o){
+			return o.x==(T=this).x&&o.y==T.y
+		}
+
+		l=f.length-1;
+		h={x:f[l].x,y:f[l].y};
+
 		beginPath();
 		L=lineWidth=10;
 
 		h.x+=g==2?L:g==4&&-L;
 		h.y+=g==3?L:g==1&&-L;
 
-		t=function(o){
-			return o.x==(T=this).x&&o.y==T.y
-		}
+		if(F.filter(t,h).length||f.filter(t,h).length)clearInterval(v)
 
-		if(F.filter(t,h).length||f.filter(t,h).length)clearInterval(v);
+		f.push({x:h.x,y:h.y});
 
 		strokeStyle=_=createLinearGradient(0,0,800,0);
 		_[$="addColorStop"](b,"magenta");
@@ -40,6 +43,8 @@ with(c.getContext("2d")){
 		for(i=f.length-1;i--;){
 			lineTo(f[i].x,f[i].y)
 		}
-		stroke()
+		stroke();
+
+
 	}
 }
