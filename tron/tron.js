ee712af5d933cbe72601bc866de440c0cd88c2f0
misantronic <david.skx@posteo.de>
2014-10-19 16:59:09 +0200

    added point system

--- tron/tron.js
+++ tron/tron.js
@@ -1,22 +1,25 @@
-w=[{x:795,y:400}];d=1;// player 1
-W=[{x:5,y:0}];D=3;// player 2
+X=a=A=0;
 with(c.getContext("2d")){
 	onkeyup=function(e){
-		(k=e.which)==39?d=2:k==37?d=4:k==38?d=1:k==40?d=3:k==87?D=1:k==68?D=2:k==83?D=3:k==65&&(D=4)
+		(k=e.which)==39?d=2:k==37?d=4:k==38?d=1:k==40?d=3:k==87?D=1:k==68?D=2:k==83?D=3:k==65?D=4:k==32&&X&&(S())
 	};
 
-	v=setInterval('\
-		c.width=c.width;\
-		z(w,d,W,"blue",0);\
-		z(W,D,w,"red",1)\
-	',60);
+	S=function(){
+		w=[{x:795,y:400}];d=1;// player 1
+		W=[{x:5,y:0}];D=3;// player 2
+		v=setInterval('\
+			c.width=c.width;\
+			p1[H="innerHTML"]=(A+=z(w,d,W,"blue",0,A));\
+			p2[H]=(a+=z(W,D,w,"red",1,a));\
+		',60)
+	};S();
 
 	// f: walls
 	// g: direction
 	// F: enemy wall
 	// c: color
 	// b: start index (for gradient)
-	z=function(f,g,F,c,b){
+	z=function(f,g,F,c,b,p){
 		t=function(o){
 			return o.x==(T=this).x&&o.y==T.y
 		}
@@ -30,7 +33,11 @@ with(c.getContext("2d")){
 		h.x+=g==2?L:g==4&&-L;
 		h.y+=g==3?L:g==1&&-L;
 
-		if(F.filter(t,h).length||f.filter(t,h).length)clearInterval(v)
+		if(F.filter(t,h).length||f.filter(t,h).length){
+			clearInterval(v)
+			X=1;
+			return 1
+		}
 
 		f.push({x:h.x,y:h.y});
 
@@ -44,7 +51,6 @@ with(c.getContext("2d")){
 			lineTo(f[i].x,f[i].y)
 		}
 		stroke();
-
-
+		return 0
 	}
 }
