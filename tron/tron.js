0d7d2f20125dcf5b2a06d1f30c0fc324b57ea5d8
misantronic <david.skx@posteo.de>
2014-10-19 17:13:42 +0200

    minifying

--- tron/tron.js
+++ tron/tron.js
@@ -1,10 +1,11 @@
-X=a=A=0;
+a=A=0;
 with(c.getContext("2d")){
 	onkeyup=function(e){
 		(k=e.which)==39?d=2:k==37?d=4:k==38?d=1:k==40?d=3:k==87?D=1:k==68?D=2:k==83?D=3:k==65?D=4:k==32&&X&&(S())
 	};
 
 	S=function(){
+		X=0;
 		w=[{x:795,y:400}];d=1;// player 1
 		W=[{x:5,y:0}];D=3;// player 2
 		v=setInterval('\
@@ -22,7 +23,7 @@ with(c.getContext("2d")){
 	z=function(f,g,F,c,b,p){
 		t=function(o){
 			return o.x==(T=this).x&&o.y==T.y
-		}
+		};
 
 		l=f.length-1;
 		h={x:f[l].x,y:f[l].y};
@@ -34,9 +35,8 @@ with(c.getContext("2d")){
 		h.y+=g==3?L:g==1&&-L;
 
 		if(F.filter(t,h).length||f.filter(t,h).length){
-			clearInterval(v)
-			X=1;
-			return 1
+			clearInterval(v);
+			return X=1
 		}
 
 		f.push({x:h.x,y:h.y});
