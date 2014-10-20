a617ed09661b1a40650a4259534074ebf581da53
misantronic <david.skx@posteo.de>
2014-10-20 19:11:24 +0200

    saved a lot of bytes by using for instead of another function

--- scriptLoader/loader.js
+++ scriptLoader/loader.js
@@ -1,16 +1,12 @@
 function l(
-	a,	// array containg url to scripts and css-files
+	a,	// array containg url to scripts
 	c	// callback
 	){
-	with(document)									// set document context
-		i=0,										// set counter i to 0
-		!function k(){								// self-invoking function to load a script
-			head.appendChild						// append to head
-					(f=createElement('script'))		// create script element f...
-				.src=a[i++];						// ... and assign the source url
-			f.onload=								// when the script is loaded
-				function(){
-					a[i]?k():c()					// look for next script in array, else call callback function
-				}
-		}()
+	with(document)											// set document context
+		for(												// parse through array
+			;(f=createElement('script')).src = a.shift()	// - create script element by shifting the array
+			;head.appendChild(f))							// - append to head
+			f.onload=function(){							// - when script is loaded...
+				c(this)										// - ... invoke callback
+			}
 }
