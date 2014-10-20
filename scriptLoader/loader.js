46e11053c6c3730b799462492dd66c42e018d155
misantronic <david.skx@posteo.de>
2014-10-20 19:16:09 +0200

    callback is now optional

--- scriptLoader/loader.js
+++ scriptLoader/loader.js
@@ -1,12 +1,12 @@
 function l(
 	a,	// array containg url to scripts
-	c	// callback
+	c	// optional callback
 	){
 	with(document)											// set document context
 		for(												// parse through array
 			;(f=createElement('script')).src = a.shift()	// - create script element by shifting the array
 			;head.appendChild(f))							// - append to head
 			f.onload=function(){							// - when script is loaded...
-				c(this)										// - ... invoke callback
+				c&&c(this)									// - ... when set, invoke callback
 			}
 }
