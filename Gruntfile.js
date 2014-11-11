b4268e04736d772d5103130fe997686e48215c31
misantronic <david.skx@posteo.de>
2014-11-11 01:10:31 +0100

    uglified a few projects, added grunt/package.json

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+				mangle: false
+			},
+
+			projects: {
+				files: {
+					'hyperhydration/hyperhydration.min.js'	: ['hyperhydration/hyperhydration.js'],
+					'invasion/invasion.min.js'				: ['invasion/invasion.js']
+				}
+			}
+		}
+	});
+
+	// load tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// tasks
+	grunt.registerTask('default', ['uglify']);
+};
