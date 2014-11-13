996eb4e9b041f4e2719fbc18244aa50459b92284
misantronic <david.skx@posteo.de>
2014-11-13 19:36:32 +0100

    added context

--- tript/tript.js
+++ tript/tript.js
@@ -1,12 +1,13 @@
 /**
  *
  * @param {String} t template
- * @param {String} r placeholder for replace function
+ * @param {Object} [c] context
+ * @param {String} [r] placeholder for replace function
  * @returns {String}
  * @constructor
  */
-function T(t, r) {
-	return eval("var _='';" + t[r = "replace"](/(<(\w+) *.*>.*<\/\2>)|(<\w+ *.*>)|(<\/\w+>)/g, function(p, a, b, c, d) {
-		return '_+="'+ (a || c || d || "")[r](/"/g, '\\"')[r](/<%(.*)%>/, '"+ ($1) +"') +'";';
-	}));
+function T(t, c, r) {
+	return eval("with(c){var _='';" + t[r = "replace"](/(<(\w+) *.*>.*<\/\2>)|(<\w+ *.*>)|(<\/\w+>)/g, function(p, a, b, c, d) {
+		return '_+="'+ (a || c || d || "")[r](/"/g, '\\"')[r](/<%(.*?)%>/g, '"+($1)+"') +'";';
+	}) + "}");
 }
