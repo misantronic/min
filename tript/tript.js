144ac1acb4927611b0561872c32b9c3bf3b14866
misantronic <david.skx@posteo.de>
2014-11-13 19:43:17 +0100

    formatting and made context optional

--- tript/tript.js
+++ tript/tript.js
@@ -1,5 +1,5 @@
 /**
- *
+ * Tript template Engine
  * @param {String} t template
  * @param {Object} [c] context
  * @param {String} [r] placeholder for replace function
@@ -7,7 +7,16 @@
  * @constructor
  */
 function T(t, c, r) {
-	return eval("with(c){var _='';" + t[r = "replace"](/(<(\w+) *.*>.*<\/\2>)|(<\w+ *.*>)|(<\/\w+>)/g, function(p, a, b, c, d) {
-		return '_+="'+ (a || c || d || "")[r](/"/g, '\\"')[r](/<%(.*?)%>/g, '"+($1)+"') +'";';
+	return eval(
+		// set context
+		"with(c||{}){var _='';" +
+		// match html-tags
+		t[r = "replace"](/(<(\w+) *.*>.*<\/\2>)|(<\w+ *.*>)|(<\/\w+>)/g, function(p, a, b, c, d) {
+			return '_+="'+
+				(a || c || d)
+					// replace quotes
+					[r](/"/g, '\\"')
+					// inline javascript
+					[r](/<%(.*?)%>/g, '"+($1)+"') +'";';
 	}) + "}");
 }
