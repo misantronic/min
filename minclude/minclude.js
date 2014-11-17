bb4064151d853d81e9eb82b48be540cff1c20e54
misantronic <david.skx@posteo.de>
2014-11-17 18:06:44 +0100

    added alternatives

--- minclude/minclude.js
+++ minclude/minclude.js
@@ -1,30 +1,32 @@
 /**
  *
- * @param {Array|String} a
- * @param {String} [c]
+ * @param {Array|String} a array or string containing script(s)
+ * @param {String} [c] base url / path
+ * @param {Array|String} [d] alternative paths
  * @param [i] placeholder
  * @returns {Minclude}
  * @constructor
  */
-Minclude = function(a, c, i) {
+Minclude = function(a, c, d, i) {
 	if(a.big) a = [a];
+	if(d.big) d = [d];
+
 	return new Promise(
 		function(r) {
+			i = 0;
 			/**
 			 *
 			 * @param {String} d
 			 * @param [e] placeholder
 			 */
-			function l(d, e){
+			!function l(g, e){
 				with(document)
-					(e=createElement('script')).src = ((c ? c +'/'+ d : d) + ".js").replace(/https*:/, ""),
+					(e=createElement('script')).src = ((c && g === a ? c +'/'+ g[i] : g[i]) + ".js").replace(/https*:/, ""),
 					head.appendChild(e),
-					e.onload=e.onerror=function() {
-						a[++i] ? l(a[i]) : r()
+					e.onload=e.onerror=function(e) {
+						e.type == 'error' && d[i] ? l(d) : a[++i] ? l(a, i) : r()
 					}
-			}
-
-			l(a[i=0])
+			}(a)
 		}
 	)
 }
