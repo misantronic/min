9de5b553d34a48e36015583344005c5602ad8d3e
misantronic <david.skx@posteo.de>
2014-11-17 17:28:57 +0100

    return promise

--- minclude/minclude.js
+++ minclude/minclude.js
@@ -1,29 +1,29 @@
 /**
  *
  * @param {Array} a
- * @param {Function} [b]
  * @param {String} [c]
  * @param [i] placeholder
  * @returns {Minclude}
  * @constructor
  */
-Minclude = function(a, b, c, i) {
-
-	/**
-	 *
-	 * @param {String} d
-	 * @param [e] placeholder
-	 */
-	function l(d, e){
-		d = c ? c +'/'+ d : d;
-
-		with(document)
-			(e=createElement('script')).src=d,
-			head.appendChild(e),
-			e.onload=e.onerror=function() {
-				a[++i] ? l(a[i]) : b && b()
+Minclude = function(a, c, i) {
+	return new Promise(
+		function(r) {
+			/**
+			 *
+			 * @param {String} d
+			 * @param [e] placeholder
+			 */
+			function l(d, e){
+				with(document)
+					(e=createElement('script')).src = ((c ? c +'/'+ d : d) + ".js").replace(/https*:/, ""),
+					head.appendChild(e),
+					e.onload=e.onerror=function() {
+						a[++i] ? l(a[i]) : r()
+					}
 			}
-	}
 
-	l(a[i=0])
+			l(a[i=0])
+		}
+	)
 }
