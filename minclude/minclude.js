5ea5a614007e01ba5fb322fc17c0b820d28f3c44
misantronic <david.skx@posteo.de>
2014-11-21 01:33:03 +0100

    added some comments

--- minclude/minclude.js
+++ minclude/minclude.js
@@ -8,6 +8,10 @@
  */
 Minclude = function(a, b, i) {
 	return (function _(r) {
+		/**
+		 * Success method when all scripts are loaded
+		 * @param {Function} c callback
+		 */
 		_.done = function(c) {
 			r = c
 		}
@@ -17,16 +21,14 @@ Minclude = function(a, b, i) {
 		 * @param {String} c
 		 * @param [d] placeholder
 		 */
-		function l(c, d){
+		!function l(c, d){
 			with(document)
 				(d=createElement('script')).src = c.replace(/https*:/, ""),
 				head.appendChild(d),
 				d.onload=d.onerror=function(e) {
 					e.type == 'error' && b[i] ? l(b[i]) : a[++i] ? l(a[i]) : r&&r()
 				}
-		}
-
-		l(a[i=0])
+		}(a[i=0])
 
 		return _
 	})()
